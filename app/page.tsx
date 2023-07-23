824b379cb67b0de902d26e630b0fb076fa3e0877
AlexMNet <emtamaldonado@gmail.com>
2023-07-22 20:59:35 -0700

    Add basic landing page, navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
-import { ModeToggle } from '@/components/mode-toggle';
 import { Typography } from '@/components/ui/typography';
+import { SpotifyBanner } from '@/components/spotify-banner';
+import { SocialMedia } from '@/components/social-media';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center">
-      <Typography variant="h1" weight="light">
-        Hello World
-      </Typography>
-      <ModeToggle />
+    <main className="h-100 flex flex-col w-full max-w-6xl mx-auto items-center justify-start px-4">
+      <div className="flex justify-center items-center flex-col text-start ">
+        <article className="max-w-lg">
+          <Typography variant="h6" weight="light" className="text-blue-500">
+            Software Developer
+          </Typography>
+          <Typography variant="h1" weight="bold">
+            Alex Maldonado
+          </Typography>
+          <Typography variant="p" weight="light">
+            I am a software developer based in Southern California. I love to
+            build user experiences that are intuitive, simple and beautiful.
+          </Typography>
+          <Typography variant="p" weight="light">
+            I currently work fulltime for Sandals Church where I build new and
+            maintain existing React and Next.js applications.
+          </Typography>
+
+          <SocialMedia />
+          <SpotifyBanner />
+        </article>
+      </div>
     </main>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,33 @@
+import { ModeToggle } from './mode-toggle';
+export function Navbar() {
+  return (
+    <>
+      <div className="w-full h-4 dark:bg-slate-500 bg-black"></div>
+      <div className="mx-auto w-full max-w-5xl py-12 px-4">
+        <nav className="flex items-center justify-center sm:justify-between">
+          <div className="hidden sm:block">AlexMaldonado.DEV</div>
+          <div className="flex items-center space-x-3">
+            <ul className="flex space-x-3">
+              <li>
+                <a href="">
+                  <span className="text-blue-500">.00</span> Home
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span className="text-blue-500">.01</span> Work
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span className="text-blue-500">.02</span> Contact
+                </a>
+              </li>
+            </ul>
+            <ModeToggle />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}

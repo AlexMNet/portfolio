232460d23b0ccc4ee020ea42cc7699f0fd393048
AlexMNet <emtamaldonado@gmail.com>
2023-11-06 11:08:30 -0800

    add splash screen

--- components/splash-screen.tsx
+++ components/splash-screen.tsx
@@ -1,33 +1,45 @@
 'use client';
 import anime from 'animejs/lib/anime.es.js';
-import { useState, useEffect, useCallback, useRef } from 'react';
-import { Typography } from './ui/typography';
+import { useState, useEffect, useCallback } from 'react';
+import { SiteTitleSVG } from './svgs';
 
 export function SplashScreen({ finishLoading }: { finishLoading: () => void }) {
   const [isMounted, setIsMounted] = useState(false);
-  const textRef = useRef(null);
 
   const animate = useCallback(() => {
     const loader = anime.timeline({
       complete: () => finishLoading(),
     });
 
-    loader.add({
-      targets: '.text',
-      opacity: [0, 1],
-      duration: 1500,
-      easing: 'easeInOutExpo',
-      endDelay: 500,
-    });
+    loader
+      .add({
+        targets: '.st0, .st1',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 500,
+        delay: function (el, i) {
+          return i * 100;
+        },
+        direction: 'normal',
+      })
+      .add({
+        targets: '.st0, .st1',
+        delay: function (el, i) {
+          return i * 50;
+        },
+        fill: '#3b82f6',
+      })
+      .add({
+        targets: '.st0, .st1',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        opacity: 0,
+        delay: function (el, i) {
+          return i * 25;
+        },
+        endDelay: 10,
+      });
   }, [finishLoading]);
-
-  const SpannedText = (text: string) => {
-    return Array.from(text).map((letter) => (
-      <span key={letter} className="letter text-4xl">
-        {letter}
-      </span>
-    ));
-  };
 
   useEffect(() => {
     const timeout = setTimeout(() => setIsMounted(true), 10);
@@ -37,9 +49,10 @@ export function SplashScreen({ finishLoading }: { finishLoading: () => void }) {
   }, [animate]);
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      {/* {SpannedText("Alex Maldonado")} */}
-      <h1 className="text text-7xl opacity-0">Alex Maldonado</h1>
+    <div className="flex h-screen items-center justify-center flex-col ">
+      <div className="flex w-full h-16 px-4 md:px-0">
+        <SiteTitleSVG className="st0 st1" />
+      </div>
     </div>
   );
 }

8c1663ef8040146c4c663e786c25196d595486b6
AlexMNet <emtamaldonado@gmail.com>
2023-11-17 01:05:17 -0800

    add message skelton when loading messages

--- app/(site)/contact/components/guestbook.tsx
+++ app/(site)/contact/components/guestbook.tsx
@@ -23,6 +23,7 @@ import fetcher from '@/lib/fetcher';
 import { Typography } from '@/components/ui/typography';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@radix-ui/react-dropdown-menu';
+import MessageSkeleton from './MessageSkeleton';
 
 export default function Guestbook() {
   const { data: session } = useSession();
@@ -157,54 +158,58 @@ export default function Guestbook() {
         )}
         <Separator />
         <ScrollArea className="mt-6 h-[650px] max-w-lg">
-          {messages?.map(
-            ({
-              id,
-              name,
-              message,
-              email,
-              createdAt,
-            }: {
-              id: string;
-              name: string;
-              message: string;
-              email: string;
-              createdAt: string;
-            }) => (
-              <div
-                key={id}
-                className="p-2 dark:bg-neutral-900 bg-gray-200 rounded-md max-w-lg my-4"
-              >
-                <div className="text-sm mb-1 flex items-center gap-1">
-                  <Typography variant="smallText" weight="bold">
-                    {name}
-                  </Typography>
-                  <div> • </div>
-                  <Typography variant="xsText" weight="light">
-                    {new Date(createdAt).toLocaleDateString('en-US', {
-                      day: '2-digit',
-                      year: '2-digit',
-                      month: 'long',
-                      weekday: 'short',
-                    })}
-                  </Typography>
-                  {session?.user?.email === email && (
-                    <button onClick={() => deleteMessage(id)}>
-                      <Trash className="w3 h-3 stroke-red-500" />
-                    </button>
-                  )}
+          {messages ? (
+            messages.map(
+              ({
+                id,
+                name,
+                message,
+                email,
+                createdAt,
+              }: {
+                id: string;
+                name: string;
+                message: string;
+                email: string;
+                createdAt: string;
+              }) => (
+                <div
+                  key={id}
+                  className="p-2 dark:bg-neutral-900 bg-gray-200 rounded-md max-w-lg my-4"
+                >
+                  <div className="text-sm mb-1 flex items-center gap-1">
+                    <Typography variant="smallText" weight="bold">
+                      {name}
+                    </Typography>
+                    <div> • </div>
+                    <Typography variant="xsText" weight="light">
+                      {new Date(createdAt).toLocaleDateString('en-US', {
+                        day: '2-digit',
+                        year: '2-digit',
+                        month: 'long',
+                        weekday: 'short',
+                      })}
+                    </Typography>
+                    {session?.user?.email === email && (
+                      <button onClick={() => deleteMessage(id)}>
+                        <Trash className="w3 h-3 stroke-red-500" />
+                      </button>
+                    )}
+                  </div>
+                  <div>
+                    <Typography
+                      variant="smallText"
+                      weight="light"
+                      className="m-0 font-thin"
+                    >
+                      {message}
+                    </Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography
-                    variant="smallText"
-                    weight="light"
-                    className="m-0 font-thin"
-                  >
-                    {message}
-                  </Typography>
-                </div>
-              </div>
+              )
             )
+          ) : (
+            <MessageSkeleton count={3} />
           )}
         </ScrollArea>
       </div>
